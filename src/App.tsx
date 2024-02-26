d09f141789f87be6abe8fd46972f3c77ead2103c
@d.shurashov <d.shurashov@gmail.com>
2024-02-26 18:35:33 +0300

    Добавлена яндекс метрика

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/order */
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { SnackbarProvider } from 'notistack'
 import { BrowserRouter } from 'react-router-dom'
@@ -6,11 +7,13 @@ import { Router } from './lib/Router'
 import { lightTheme } from './lib/thems'
 
 import './App.css'
+import { YMInitializer } from 'react-yandex-metrika'
 
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
+      <YMInitializer accounts={[96580340]} options={{ webvisor: true }} />
       <section>
         <BrowserRouter>
           <SnackbarProvider>
--- src/lib/Router.tsx
+++ src/lib/Router.tsx
@@ -1,8 +1,19 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import ym from 'react-yandex-metrika'
 
 import { CompetitionPage, OperatingPage } from '../pages'
 
 export const Router = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    if (pathname === '/') {
+      ym('96580340', 'reachGoal', 'opearatingIsVisited')
+    }
+  }, [pathname])
+
   return (
     <Routes>
       <Route path="/" element={<OperatingPage />} />
